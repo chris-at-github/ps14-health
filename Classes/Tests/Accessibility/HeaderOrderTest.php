4d4f1e827071a0b5c29b17387d41d52c381477f5
Christian Pschorr <c.pschorr@datamints.com>
2021-03-01 23:45:51 +0100

    Test für die Überprüfung der Überschriftenstruktur erstellt

--- Classes/Tests/Accessibility/HeaderOrderTest.php
+++ Classes/Tests/Accessibility/HeaderOrderTest.php
@@ -16,9 +16,33 @@ class HeaderOrderTest extends UriTest implements UriTestInterface {
 	 */
 	public function perform() {
 		$html = $this->getUriResponse()->getBody();
+		$references = [];
 
 		if(preg_match_all('/<h([1-6]).*?>.*?<\/h[1-6]>/msi', $html, $matches) !== 0) {
-			DebuggerUtility::var_dump($matches);
+
+			// Speicherung der vorherigen Ueberschriften-Stufe
+			$prev = 0;
+
+			for($i = 0; $i < count($matches[0]); $i++) {
+				$current = (int) $matches[1][$i];
+
+				if($current > $prev) {
+
+					// Abstand berechnen
+					$distance = $current - $prev;
+
+					// Bei einem Abstand mehr eins ist ein Sprung enthalten
+					if($distance > 1) {
+						$references[] = $matches[0][$i];
+					}
+				}
+
+				$prev = $current;
+			}
+		}
+
+		if(empty($references) === false) {
+			return new ErrorTestResult('Incorrect ' . count($references) . ' headline orderings found ' . implode(', ', $references));
 		}
 
 		return new SuccessTestResult();
